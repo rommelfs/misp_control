9ba4017965f9bb9016293b4ac0a5fe32fb904184
Sascha Rommelfangen <sascha@rommelfangen.de>
2026-03-17 13:24:27 +0100

    various improvements

--- upgrade_misp.sh
+++ upgrade_misp.sh
@@ -4,7 +4,7 @@
 # config file needed (config.csv)
 # fields:
 # server_name,API_key
-# 2024-03 Sascha Rommelfangen, CIRCL, LHC
+# 2024-03 - 2026 Sascha Rommelfangen, CIRCL, LHC
 
 function update_server { 
   HOST="$1"
@@ -12,7 +12,7 @@ function update_server {
   ../matrix.sh/matrix.sh "Currently updating MISP server $HOST. Cross your fingers!"
   RESULT=$(curl -s -d '[]' -H "Authorization: $KEY" -H "Accept: application/json" -H "Content-type: application/json" -X POST https://$HOST/servers/update )
   #echo "$RESULT"
-  if [[ $(echo $RESULT|jq|egrep -e "status|Update failed"|grep -v 0) ]]
+  if [[ $(echo $RESULT|jq|egrep -e "status\"\:|Update failed"|grep -v 0) ]]
   then
       echo -en "  => Something went wrong updating MISP. See the output? (y/n): "
       read -r -s -n 1 input
@@ -24,35 +24,32 @@ function update_server {
 		"n")
 			;;
 	  esac
-      RESULT=$(curl -s -d '[]' -H "Authorization: $KEY" -H "Accept: application/json" -H "Content-type: application/json" -X POST https://$HOST/servers/updateJSON/ )
-      if [[ $(echo $RESULT|jq|egrep -e "status|Update failed"|grep -v 0) ]]
-      then
-          echo -en "  => Something went wrong updating JSON. See the output? (y/n): "
-          read -r -s -n 1 input
-          echo $input
-          case "$input" in
-		     "y")
-                echo ${RESULT} | jq
-			    ;;
-		    "n")
-			    ;;
-	      esac
-      else
-          echo "  => Updated JSON successfully or no update available"
-      fi
   else
       echo "  => Updated MISP successfully or no update available"
+  fi
+  echo "Updating JSON"
+  RESULT=$(curl -s -d '[]' -H "Authorization: $KEY" -H "Accept: application/json" -H "Content-type: application/json" -X POST https://$HOST/servers/updateJSON/ )
+  if [[ $(echo $RESULT|jq|egrep -e "status\"\:|Update failed"|grep -v 0) ]]
+  then
+      echo -en "  => Something went wrong updating JSON. See the output? (y/n): "
+      read -r -s -n 1 input
+      echo $input
+      case "$input" in
+        "y")
+            echo ${RESULT} | jq
+	        ;;
+	    "n")
+		    ;;
+	  esac
+  else
+      echo "  => Updated JSON successfully or no update available"
   fi
 }
 
 # get release version
-#VERSION_RELEASE=$( curl -s -I https://github.com/MISP/MISP/releases/latest |grep location |head -n 8|ggrep -oP '/MISP/MISP/releases/tag/\Kv[0-9]+\.[0-9]+\.[0-9]+' | rev | cut -d "/" -f 1|rev|cut -d "v" -f 2 )
-#VERSION_RELEASE="${VERSION_RELEASE%%[[:cntrl:]]}"
 VERSION_RELEASES=$( curl -s https://github.com/MISP/MISP/releases |ggrep -oP '/MISP/MISP/releases/expanded_assets/\Kv[0-9]+\.[0-9]+\.[0-9]+' | rev | cut -d "/" -f 1|rev|cut -d "v" -f 2 )
 VERSION_RELEASE=""
 # get tag version
-#VERSION_TAG=$( curl -s https://github.com/MISP/MISP/tags|grep /MISP/MISP/releases/tag/|head -n 1|rev|cut -d "<" -f 3|cut -d ">" -f 1|rev|cut -d "v" -f 2 )
-#VERSION_TAG="${VERSION_TAG%%[[:cntrl:]]}"
 VERSION_TAGS=$( curl -s https://github.com/MISP/MISP/tags/ |grep /MISP/MISP/releases/tag/|head -n 18|ggrep -oP '/MISP/MISP/releases/tag/\Kv[0-9]+\.[0-9]+\.[0-9]+' | rev|cut -d "<" -f 3|cut -d ">" -f 1|rev|cut -d "v" -f 2 )
 VERSION_TAG=""
 CONFIG="config.csv"
@@ -93,7 +90,6 @@ do
         VERSION_TAG=$( echo "$VERSION_TAGS" | egrep "2.5.*" | cut -d " " -f 1 | head -n 1)
         VERSION_RELEASE=$( echo "$VERSION_RELEASES" | egrep "2.5.*" | cut -d " " -f 1 | head -n 1)
     fi
-    #VERSION_TAG="${VERSION_TAG%%[[:cntrl:]]}"
     printf "%25s\t%12s\t%12s\t%12s" ${HOST} ${VERSION_RUNNING} ${VERSION_TAG} ${VERSION_RELEASE}
     if [[ ${VERSION_RUNNING} == ${VERSION_TAG} ]]
         # || ${VERSION_RUNNING} == ${VERSION_RELEASE} ]]
